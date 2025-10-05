e54b012fe52363650e753f15396b5bcf9cc26334
Anton Bezmelnitsin <a.bezmelnitsin.a@yandex.ru>
2025-10-05 17:53:46 +0500

    Initial commit

---
+++ src/main/java/ru/clonsaldafon/spring_warranty_cards/SpringWarrantyCardsApplication.java
@@ -0,0 +1,13 @@
+package ru.clonsaldafon.spring_warranty_cards;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringWarrantyCardsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringWarrantyCardsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/clonsaldafon/spring_warranty_cards/SpringWarrantyCardsApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.clonsaldafon.spring_warranty_cards;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringWarrantyCardsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
